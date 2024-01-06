49489a1f2be6539727cd43956ea0392e5164ba36
Cameron <132536746+cameronjobson@users.noreply.github.com>
2024-01-06 22:51:45 +0000

    Jan 6 2023 16:50

--- src/App.js
+++ src/App.js
@@ -37,13 +37,17 @@ function NeoTool() {
 
   const formatDate = (date) => {
     // Ensure we're formatting the date as UTC to avoid timezone conversions
-    return new Date(date.getTime() + (date.getTimezoneOffset() * 60000))
+    const formattedDate = new Date(date.getTime() + (date.getTimezoneOffset() * 60000))
       .toLocaleDateString('en-US', {
         year: 'numeric',
         month: '2-digit',
         day: '2-digit',
       });
+  
+    // Wrap the formatted date in HTML <strong> tags to make it bold
+    return `<strong>${formattedDate}</strong>`;
   };
+  
 
 
   const calculateTreatmentDate = (dateOfBirth, daysOffset) => {
@@ -72,8 +76,26 @@ function NeoTool() {
     };
   };
 
+  //DayInputTraditionalTreatmentDate
+
+  const dayInputTraditionalTreatmentDate = (treatmentDescription, CGA_days, source) => {
+    if (!dateOfBirth) return '';
+  
+    const dob = new Date(dateOfBirth);
+    const treatmentDate = new Date(
+      dob.getTime() + CGA_days * 24 * 60 * 60 * 1000 - (gestAgeTotalDays * 24 * 60 * 60 * 1000)
+    );
+  
+    return {
+      description: `${treatmentDescription} ${formatDate(treatmentDate)}`,
+      date: formatDate(treatmentDate),
+      source: source,
+    };
+  };
+  
+
   //Calculate ROP Dates
-  const calculateRopExamDate = () => {
+  const calculateRopExamDate = (R) => {
     if (gestAgeTotalDays < 0) {
       // Handle negative gestAgeTotalDays, if needed
       return; // or throw an error
@@ -99,8 +121,13 @@ function NeoTool() {
     } else if (gestAgeTotalDays >= 252) {
       weekNumber = 40;
     }
+    if (R === false){
+      return traditionalTreatmentDate("<b>ROP</b> First Exam due near week of", weekNumber);
+    }
+    else {
+      return traditionalTreatmentDate("<b>ROP</b> First Exam (if risk factors) due near week of", weekNumber);
+    }
 
-    return traditionalTreatmentDate("<b>ROP</b> First Exam Due near week of:", weekNumber);
   }
 
   // For treatment dates determined by Days Of Life
@@ -133,48 +160,150 @@ function NeoTool() {
     if (weight > 0 && gestAgeTotalDays > 0) {
 
       //Donor Breast Milk
-      if (weight <= 1500){
-        treatments.push(plainText('<b>Donor Breast Milk</b>', "https://publications.aap.org/pediatrics/article/139/1/e20163440/52000/Donor-Human-Milk-for-the-High-Risk-Infant"))
+      if (weight < 1500 && gestAgeTotalDays < 245){
+        treatments.push(traditionalTreatmentDate('<b>DBM</b> stop at 1500g and 35w', 35))
+      }
+      if (weight < 1500 && gestAgeTotalDays >= 245){
+        treatments.push(plainText('<b>DBM</b> stop at 1500g'))
+      }
+      if (weight >= 1500 && gestAgeTotalDays < 245){
+        treatments.push(traditionalTreatmentDate('<b>DBM</b> stop at 1500g and 35w', 35))
+      }
+
+      //Prolacta
+      if (weight <= 1250){
+        treatments.push(traditionalTreatmentDate('<b>Prolacta</b> stop at 1500g and 33w', 33))
+      }
+
+      //HMF/PTF
+      if (weight >= 1251 && weight <= 2200){
+        treatments.push(traditionalTreatmentDate('<b>HMF/PTF</b> until 3.5kg then D/C feeds ', 35))
       }
       //Multivitamin
       treatments.push(simpleTreatment('<b>MVI/Fe</b> at full feeds and >/=14dol', 14));
 
-      //ROP exam
-      treatments.push(calculateRopExamDate())
-
-      //Head Ultrasound
-      if (gestAgeTotalDays <= 210){
-        treatments.push(plainText("<b>Head Ultrasound ▼</b>", "https://publications.aap.org/pediatrics/article/146/5/e2020029082/75330/Routine-Neuroimaging-of-the-Preterm-Brain?autologincheck=redirected"))
-        treatments.push(simpleTreatment("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;First HUS date @ 7 days:", 7))
-        treatments.push(simpleTreatment("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Second HUS date @ ≈ 1 month:", 30,))
-        treatments.push(traditionalTreatmentDate('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Third HUS date @ 1 term or discharge:', 40,))
+      //NIPPV
+      if (gestAgeTotalDays <= 209){
+        treatments.push(plainText("<b>NIPPV</b> on admission"))
       }
 
+      //Trial of cpap *NEEDS WORK*
+
+      //Oxygen Challenge Test
+      if (gestAgeTotalDays < 223){
+        treatments.push(dayInputTraditionalTreatmentDate('<b>Oxygen Challenge Test</b> done on this night', 251))
+      }
+      
+
       //Postnatal Steroids
-      if (gestAgeTotalDays <= 210){
-        treatments.push(plainText("<b>Postnatal Steroids BPD Calc▼</b>", "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7578580/"))
-        treatments.push(simpleTreatment("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;First @ 14 days:", 14))
-        treatments.push(simpleTreatment("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Second @ 21 days:", 21))
-        treatments.push(simpleTreatment("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Second @ 28 days:", 28))
+      if (gestAgeTotalDays <= 209){
+        treatments.push(plainText("<b>PNS</b>  mod/sev BPD risk>60% at 14, 28d if on vent"))
+        treatments.push(simpleTreatment("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;14 days:", 14))
+        treatments.push(simpleTreatment("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;28 days:", 28))
+      }
+
+      //Caffeine *NEEDS WORK*
+      if(gestAgeTotalDays <= 216 || weight < 1500){
+        treatments.push(plainText("<b>Caffeine</b>"))
+        //First Protocal
+        if(gestAgeTotalDays >= 154 && gestAgeTotalDays <= 174){
+          treatments.push(simpleTreatment("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10mg/kg/d on admit increase to 20mg/kg/d dol 8 OR 24h prior to extubation if <8d", 8))
+        }
+        //INPUT 2ND CAFFEINE PROTOCAL
+
+        //Third Protocal
+        if((gestAgeTotalDays >= 175 && gestAgeTotalDays <= 216) || (gestAgeTotalDays >= 175 && weight < 1500)){
+        treatments.push(plainText("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;20mg/kg/d on admit"))
+        }
+
+        //4th Protocal
+        if((gestAgeTotalDays >= 175 && gestAgeTotalDays <= 216) || (gestAgeTotalDays >= 175 && weight < 1500)){
+          treatments.push(traditionalTreatmentDate('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Stop at 34w if off PPV & 5d apnea free (off PPV: off CPAP & on <4lpm HFNC)', 34,))
+        }
+
+        //5th Protocal
+        if(gestAgeTotalDays <= 216 || weight < 1500){
+          treatments.push(traditionalTreatmentDate('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Stop weight adjustments at 34w', 34,))
+        }
+
+        //6th Protocal
+        if(gestAgeTotalDays <= 216 || weight < 1500){
+          treatments.push(traditionalTreatmentDate('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ok to stop at 43w if remains on PPV', 43,))
+        }
       }
 
       //Synagis
       if (gestAgeTotalDays <= 202){
-        treatments.push(plainText("<b>Synagis</b>", "https://publications.aap.org/pediatrics/article/134/2/415/33013/Updated-Guidance-for-Palivizumab-Prophylaxis-Among"))
-      }
-      
-      //Hepatitis b vaccine
-      treatments.push(plainText("<b>Hepitatis B Vaccine</b>","https://publications.aap.org/redbook/pages/Immunization-Schedules"))
-      if (weight >= 2000){
-        treatments.push(plainText("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1 dose within 24 hours of birth if medically stable"))
-      } else if (weight < 2000){
-        treatments.push(simpleTreatment("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1 dose at chronological age 1 month or hospital discharge", 30))
+        treatments.push(plainText("<b>Synagis</b> (if Beyfortus not available)"))
       }
 
-      //NIPPV
-      if (gestAgeTotalDays <= 216 && weight < 1500){
-        treatments.push(plainText("<b>NIPPV</b>", "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9874940/"))
+      if (gestAgeTotalDays >= 203 && gestAgeTotalDays <= 223){
+        treatments.push(simpleTreatment("<b>Synagis</b> (if Beyfortus not available) if O2 at least first 28d after birth ", 28))
       }
+
+      //ECHO
+      if (gestAgeTotalDays <= 224){
+        treatments.push(plainText("ECHO for PAH"))
+      }
+      if (gestAgeTotalDays <= 195){
+        treatments.push(traditionalTreatmentDate('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;@32w if remains on ventilator', 32,))
+      }
+      if (gestAgeTotalDays <= 224){
+        treatments.push(traditionalTreatmentDate('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;@36w if remains on resp support', 36,))
+      }
+
+      //Head Ultrasound
+      if (gestAgeTotalDays <= 216){
+        treatments.push(plainText("<b>HUS</b>"))
+        treatments.push(simpleTreatment("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1wk HUS (7d)", 7))
+        treatments.push(simpleTreatment("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1mo HUS (30d)", 30,))
+        treatments.push(traditionalTreatmentDate('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Term/DC HUS', 40,))
+      }
+
+      //ROP exam
+      if (weight <= 1500 || gestAgeTotalDays <= 216){
+        treatments.push(calculateRopExamDate(false))
+      }
+      if ((gestAgeTotalDays >= 217 && gestAgeTotalDays <= 230) && (weight >= 1501 && weight <= 2000)){
+        treatments.push(calculateRopExamDate(true))
+      }
+
+      //Nest
+      if (gestAgeTotalDays <= 202){
+        treatments.push(plainText("<b>Nest</b> F/U Tier 1b"))
+      }
+      if ((gestAgeTotalDays >= 203 && gestAgeTotalDays <= 223) && (weight <= 1500)){
+        treatments.push(plainText("<b>Nest</b> F/U Tier 2"))
+      }
+
+      //Hepatitis b vaccine
+      if (weight <= 1999){
+        treatments.push(simpleTreatment("<b>Hep B Vaccine</b> at 30dol", 30,))
+      }
+
+      //Fluconazole Prophylaxis
+      if (gestAgeTotalDays <= 174 && weight < 750){
+        treatments.push(plainText("<b>Fluconazole Prophylaxis:</b>"))
+        treatments.push(simpleTreatment("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;3mg/kg/dose every 72h until CVL stopped or max of 6w", 42,))
+      }
+
+      //Vit K
+      if (gestAgeTotalDays >= 154 && gestAgeTotalDays <= 174){
+        treatments.push(simpleTreatment("<b>Vit K</b> 0.3mg IV q72h x 4 doses 0.5mg IM @ 14 dol", 14,))
+      }
+
+      //TSH and Free T4:
+      if (gestAgeTotalDays <= 216 || weight <= 1500){
+        treatments.push(plainText("<b>TSH and Free T4:</b>"))
+        treatments.push(simpleTreatment("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;30 days", 30,))
+        if (gestAgeTotalDays <= 174){
+          treatments.push(simpleTreatment("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;60 days", 60,))
+        }
+
+      //cCMV screening
+      treatments.push(simpleTreatment("<b>cCMV screening</b> last day 3w old (if no hearing screen done)", 21,))
+      }
+
     }
 
     return treatments;
